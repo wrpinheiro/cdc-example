39492ebaea59830617b21949723c1a90203a6a4d
Wellington Ricardo Pinheiro <wellington.pinheiro@gmail.com>
2017-03-27 14:14:43 -0300

    Add Example file with some dead code

---
+++ Example.java
@@ -0,0 +1,15 @@
+public class Example {
+  public static void notUsed() {
+  	used();
+  }
+
+  public static void used() {
+  }
+
+  public static void main(String[] args) {
+  	int x = 2;
+
+    System.out.println("Hello " + args[0]);
+  }
+}
+
