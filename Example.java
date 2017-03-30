27c4e1e8dc0f6f78dca4f1cdd7c70606ba13c693
Wellington Ricardo Pinheiro <wellington.pinheiro@gmail.com>
2017-03-29 22:22:19 -0300

    Change example

--- Example.java
+++ Example.java
@@ -1,5 +1,11 @@
 public class Example {
     public void printHiThere() {
-        System.out.println("Hi there");
+        System.out.println(aLoudHiThere(
+            "Hi there"));
     }
+
+    private String aLoudHiThere(String msg) {
+        return "HI THERE";
+    }
+
 }
