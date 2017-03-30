8fc0592773246ce74037579c1f57ed1a349cfd95
Wellington Ricardo Pinheiro <wellington.pinheiro@gmail.com>
2017-03-29 21:08:07 -0300

    Change example

--- Example.java
+++ Example.java
@@ -1,30 +1,7 @@
-// Example is not used
 public class Example {
-    // notUsed is not used
-    public static void notUsed() {
-        used();
-    }
+    public Example() {
+      String value = "Hi there";
 
-    // used is really used!
-    public static void used() {
-    }
-
-    private int unusedPrivateMethod() {
-    }
-
-    private int usedPrivateMethod(int i) {
-    }
-
-    // method main is not called in the code
-    // args is used!
-    public static void main(String[] args) {
-        // unusedVar is not used
-        int unusedVar = 2;
-
-        // System.out.println(unusedVar);
-
-        System.out.println("Hello " + args[0]);
-
-        new Example().usedPrivateMethod(1);
+      System.out.println(value.toUpperCase());
     }
 }
